6f3d5a26a606d8e37f96d406e09dc6519c767844
Philip Pages <philip.pages@gmail.com>
2021-02-02 17:03:55 -0600

    Computer random choice

---
+++ main.js
@@ -0,0 +1,17 @@
+<script>
+
+function computerPlay() {
+
+    const computerOptions = [`Rock`, `Paper`, `Scissors`];
+    let computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+
+    return computerChoice;
+    
+    
+    }
+
+
+
+
+
+</script>
