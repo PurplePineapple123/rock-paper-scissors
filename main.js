7ae6d133ec50a526ab6466df69a987ad2f8bd615
Philip Pages <philip.pages@gmail.com>
2021-02-04 17:20:39 -0600

    added JS to seperate file. Stated creating interface using flex. Buttons working. Need to add images in buttons, create counter, etc...

--- main.js
+++ main.js
@@ -1,17 +1,78 @@
-<script>
 
-function computerPlay() {
+            let computerScore = 0;
+            let playerScore = 0;
+            let i = 0;
 
-    const computerOptions = [`Rock`, `Paper`, `Scissors`];
-    let computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+            function computerPlay() {
 
-    return computerChoice;
-    
-    
-    }
+                const computerOptions = [`rock`, `paper`, `scissors`]
+                let randomAnswer = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+                return randomAnswer;
+            }
+
+            // need to convert this method to take input from people. For now we're getting the logic of 2 comoputers playing against each other. 
+
+            function playerPlay() {
+
+                const computerOptions = [`Rock`, `Paper`, `Scissors`]
+
+               // let randomAnswer = prompt(`Enter rock, paper, or scissors:`);
+                return randomAnswer.toLowerCase();
+            }
+
+            function playRound(playerSelection, computerSelection) {
+               
+                if (playerSelection === computerSelection) {
+                    return `You tied this round!`;
+                    
+                }   else if (playerSelection === `rock` && computerSelection === `scissors`) {
+                    playerScore++;   
+                    return `You win! Rock beats Scissors`;
+
+                }   else if (playerSelection === `scissors` && computerSelection === `rock`) {
+                    computerScore++;   
+                    return `You lose! Rock beats Scissors`;
+                
+
+                }   else if (playerSelection === `paper` && computerSelection === `scissors`) {
+                    computerScore++;   
+                    return `You lose! Scissors beats paper`;
+                
+
+                }   else if (playerSelection === `scissors` && computerSelection === `paper`) {
+                    playerScore++;     
+                    return `You win! Scissors beats Paper`;
+                }
+                
+                else if (playerSelection === `rock` && computerSelection === `paper`) {
+                    computerScore++;   
+                    return `You lose! Paper beats Rock`;
+                }
 
 
+                else if (playerSelection === `paper` && computerSelection === `rock`) {
+                    playerScore++;   
+                    return `You win! Paper beats Rock`;
+                }
+            }
 
+            
 
+            function play() {
+                    playRound();
+                    i++;
+                    console.log(playRound(playerPlay(), computerPlay()));
+                    console.log(`Computer score: ${computerScore}`);
+                    console.log(`Your score: ${playerScore}`);
 
-</script>
+                    if (i !==3) {
+                        play();
+
+                    } else {
+                    console.log(`GAME OVER`);
+                    }
+                }
+
+            
+        play();
+
